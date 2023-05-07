2580c2a43bb919e7833f459710c1065b0ce0e282
JohnGrib <johngrib82@gmail.com>
2023-05-07 17:25:49 +0900

    Ratio.toString 테스트 추가

--- src/test/kotlin/johngrib/math/RatioTest.kt
+++ src/test/kotlin/johngrib/math/RatioTest.kt
@@ -8,6 +8,48 @@ import kotlin.math.abs
 
 @DisplayName("Ratio")
 class RatioTest : DescribeSpec({
+    describe("toString") {
+        context("분자가 0 이라면") {
+            val givenDenominators = listOf(
+                1, 2, 3, 100, Int.MAX_VALUE,
+                -1, -2, -3, -100, Int.MIN_VALUE + 1
+            )
+            it("문자열 '0' 을 리턴한다") {
+                givenDenominators.forEach { denominator ->
+                    Ratio.of(numerator = 0, denominator = denominator)
+                        .toString() shouldBe "0"
+                }
+            }
+        }
+        context("더 이상 약분할 수 없는 분수라면") {
+            val givenCases = listOf(
+                Triple(3, 2, "3/2"),
+                Triple(-3, 2, "-3/2"),
+                Triple(7, 100, "7/100"),
+            )
+
+            it("분자/분모 형태의 문자열을 리턴한다") {
+                givenCases.forEach { (numerator, denominator, expected) ->
+                    Ratio.of(numerator = numerator, denominator = denominator)
+                        .toString() shouldBe expected
+                }
+            }
+        }
+        context("약분 가능한 분수라면") {
+            val givenCases = listOf(
+                Triple(3, 6, "1/2"),
+                Triple(8, 40, "1/5"),
+                Triple(80, 200, "2/5"),
+            )
+
+            it("약분된 분자/분모 형태의 문자열을 리턴한다") {
+                givenCases.forEach { (numerator, denominator, expected) ->
+                    Ratio.of(numerator = numerator, denominator = denominator)
+                        .toString() shouldBe expected
+                }
+            }
+        }
+    }
     describe("gcd") {
         val gcdParcialFirst = { number1: Int -> { number2: Int -> Ratio.gcd(number1, number2) } }
         val gcdParcialLast = { number1: Int -> { number2: Int -> Ratio.gcd(number2, number1) } }
@@ -38,7 +80,7 @@ class RatioTest : DescribeSpec({
             )
 
             it("두 수의 최대공약수를 리턴한다") {
-                givenCases.forEach { number  ->
+                givenCases.forEach { number ->
                     val resultGCD = Ratio.gcd(number, number)
 
                     assertEquals(resultGCD, abs(number), "리턴된 GCD 값이 예상한 값과 같아야 합니다.")
