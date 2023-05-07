e252a3465a50a1fffa123fbb026862229c3ddcf2
JohnGrib <johngrib82@gmail.com>
2023-05-07 17:26:12 +0900

    Ratio.toString 함수 오버라이드

--- src/main/kotlin/johngrib/math/Ratio.kt
+++ src/main/kotlin/johngrib/math/Ratio.kt
@@ -2,9 +2,45 @@ package johngrib.math
 
 import kotlin.math.abs
 
-class Ratio() {
+class Ratio
+private constructor(
+    private val _numerator: Int,
+    private val _denominator: Int,
+) {
+    private var gcdValue: Int
+    private var numerator: Int
+    private var denominator: Int
+
+    init {
+        if (_denominator == 0) {
+            throw IllegalArgumentException("Denominator cannot be 0.")
+        }
+        gcdValue = gcd(_numerator, _denominator)
+        numerator = _numerator / gcdValue
+        denominator = _denominator / gcdValue
+    }
+
+    override fun toString(): String {
+        if (numerator == 0) {
+            return "0"
+        }
+        if (denominator == 1) {
+            return "$numerator"
+        }
+        if ((numerator < 0 && denominator < 0) || (numerator > 0 && denominator > 0)) {
+            return "$numerator/$denominator"
+        }
+        if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0)) {
+            return "-${abs(numerator)}/${abs(denominator)}"
+        }
+        return "$numerator/$denominator"
+    }
 
     companion object {
+        fun of(numerator: Int, denominator: Int): Ratio {
+            return Ratio(numerator, denominator)
+        }
+
         /**
          * 두 수의 '최대공약수'를 리턴합니다.
          */
