7a86355d052ab4e8e9bc5e49904d0b8e5f714f73
JohnGrib <johngrib82@gmail.com>
2023-05-07 16:22:12 +0900

    테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
+    testImplementation("io.kotest:kotest-property-jvm:5.5.5")
 }
 
 tasks.test {
